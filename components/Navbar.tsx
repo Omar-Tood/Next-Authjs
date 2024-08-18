f73e7d9daedd10f3ec9c4783dc7071c767bf5b19
unknown <omarjibrilabdulkhadir@gmail.com>
2024-08-18 17:15:24 +0300

    creating protected Routes.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,12 +3,19 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className="border-g bg-background w-full flex items-center">
-        <div>
+    <nav className="border-b bg-background w-full flex items-center">
+        <div className="flex w-full items-center justify-between my-4">
            <Link className="font-bold" href="/">
             Home
            </Link>
         </div>
+        
+        <div>
+         <Link href="/middleware">Middleware</Link>
+         <Link href="/server">Server</Link>
+         
+        </div>
+       
     </nav>
   )
 }
