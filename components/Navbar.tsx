0aedbad7af9260582a5d1a2c5d901e6fe48adbd2
unknown <omarjibrilabdulkhadir@gmail.com>
2024-08-19 13:25:54 +0300

    setup Login page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,27 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Navbar() {
   return (
     <nav className="border-b bg-background w-full flex items-center">
-        <div className="flex w-full items-center justify-between my-4">
-           <Link className="font-bold" href="/">
-            Home
-           </Link>
-        </div>
-        
+      <div className="flex w-full items-center justify-between my-4">
+        <Link className="font-bold" href="/">
+          Home
+        </Link>
+
         <div className="flex items-center gap-x-6">
-         <Link href="/middleware">Middleware</Link>
-         <Link href="/server">Server</Link>
-         
+          <Link href="/middleware">Middleware</Link>
+          <Link href="/server">Server</Link>
         </div>
-       
+
+        <div className="flex items-center gap-x-5">
+        <Link href="/sign-in">
+          <div>Login</div>
+        </Link>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
